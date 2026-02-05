f8841faf76f88e2ba3422c855fba4133e29ce909
AnkitaSharma06 <ankitaasharma0620@gmail.com>
2026-02-06 01:09:58 +0530

    Completed

---
+++ LoginAuthenticationSystem/AuthenticationSystem.java
@@ -0,0 +1,32 @@
+package LoginAuthenticationSystem;
+
+import java.util.Scanner;
+class AuthenticationSystem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String correctUsername = "admin";
+        String correctPassword = "1234";
+        int attempts = 0;
+        while (attempts < 3) {
+            System.out.print("Enter username: ");
+            String username = sc.nextLine();
+            System.out.print("Enter password: ");
+            String password = sc.nextLine();
+            if (username.equals(correctUsername) && password.equals(correctPassword)) {
+                System.out.println("Login successful!");
+                break;
+            } 
+            else {
+                attempts++;
+                System.out.println("Invalid credentials.");
+                if (attempts < 3) {
+                    System.out.println("Attempts left: " + (3 - attempts));
+                }
+            }
+        }
+        if (attempts == 3) {
+            System.out.println("Account locked due to 3 failed login attempts.");
+        }
+        sc.close();
+    }
+}
